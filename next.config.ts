0b16cd5cb264dc89cf5d97a3144e7411bd6ff922
Kapil Kumar <145493471+kapil-maan@users.noreply.github.com>
2025-07-29 21:52:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // This line is the key. It tells Next.js to output a static site.
+  output: 'export',
+  basePath: '/lock-box-web',
 };
 
 export default nextConfig;
